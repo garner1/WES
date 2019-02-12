5f60df68782a548d9ab0241ef386962f54996d40
garner1 <silvano.garnerone@gmail.com>
2019-02-12 09:23:41 +0100

    minor

--- bin/main.sh
+++ bin/main.sh
@@ -7,7 +7,6 @@ R2=$4	                        # full path to R2 file
 
 refgen=~/Work/genomes/Homo_sapiens.GRCh37.dna.primary_assembly.fa/GRCh37.fa # full path to reference genome
 numbproc=24
-quality=30
 
 echo "Processing " ${exp}_${run} 
 datadir=/media/garner1/hdd/WES/${exp}_${run} 
@@ -15,22 +14,24 @@ mkdir -p $datadir
 echo "Aligning ..."
 if [ ! -f $datadir/$exp.bam.bai ]; then
     bwa mem -v 1 -t $numbproc $refgen $R1 $R2 > $datadir/$exp.sam
-    samtools view -h -Sb -q $quality $datadir/$exp.sam > $datadir/$exp.bam 
-    samtools sort $datadir/$exp.bam  -o $datadir/$exp.sorted.bam
+    samtools view -h -Sb $datadir/$exp.sam > $datadir/$exp.bam
+    rm $datadir/$exp.sam
+    samtools sort $datadir/$exp.bam -o $datadir/$exp.sorted.bam
     mv $datadir/$exp.sorted.bam $datadir/$exp.bam 
     samtools index $datadir/$exp.bam 
 fi
-echo "Deduplicating ..."
-if [ ! -f $datadir/$exp.bam.deduplicated.bam ]; then
-    umi_tools dedup -I $datadir/$exp.bam  --paired -S $datadir/$exp.bam.deduplicated.bam --umi-separator=: --edit-distance-threshold 2 -L $datadir/$exp.group.log --output-stats=$datadir/$exp
-fi
-if [ ! -f $datadir/$exp.bam.deduplicated.sorted.bam ]; then
-    samtools sort $datadir/$exp.bam.deduplicated.bam -o $datadir/$exp.bam.deduplicated.sorted.bam
-fi
 
-echo "Coverage calculation ..."
-if [ ! -f $datadir/$exp.hist.txt ]; then
-    bedtools coverage -hist -b $datadir/$exp.bam.deduplicated.sorted.bam -a ~/Work/pipelines/data/agilent/S07604715_Covered.woChr.bed > $datadir/$exp.hist.txt
-fi
+# echo "Deduplicating ..."
+# if [ ! -f $datadir/$exp.bam.deduplicated.bam ]; then
+#     umi_tools dedup -I $datadir/$exp.bam  --paired -S $datadir/$exp.bam.deduplicated.bam --umi-separator=: --edit-distance-threshold 2 -L $datadir/$exp.group.log --output-stats=$datadir/$exp
+# fi
+# if [ ! -f $datadir/$exp.bam.deduplicated.sorted.bam ]; then
+#     samtools sort $datadir/$exp.bam.deduplicated.bam -o $datadir/$exp.bam.deduplicated.sorted.bam
+# fi
+
+# echo "Coverage calculation ..."
+# if [ ! -f $datadir/$exp.hist.txt ]; then
+#     bedtools coverage -hist -b $datadir/$exp.bam.deduplicated.sorted.bam -a ~/Work/pipelines/data/agilent/S07604715_Covered.woChr.bed > $datadir/$exp.hist.txt
+# fi
 echo "Done!"
 echo
