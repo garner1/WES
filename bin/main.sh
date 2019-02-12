e038f74bd489be7d0e3cc5e7ccc563a7b57f7f44
garner1 <silvano.garnerone@gmail.com>
2019-02-12 15:13:54 +0100

    oneliner alignment and sorting

--- bin/main.sh
+++ bin/main.sh
@@ -13,11 +13,12 @@ datadir=/media/garner1/hdd/WES/${exp}_${run}
 mkdir -p $datadir
 echo "Aligning ..."
 if [ ! -f $datadir/$exp.bam.bai ]; then
-    bwa mem -v 1 -t $numbproc $refgen $R1 $R2 > $datadir/$exp.sam
-    samtools view -h -Sb $datadir/$exp.sam > $datadir/$exp.bam
-    rm $datadir/$exp.sam
-    samtools sort $datadir/$exp.bam -o $datadir/$exp.sorted.bam
-    mv $datadir/$exp.sorted.bam $datadir/$exp.bam 
+    bwa mem -v 1 -t $numbproc $refgen $R1 $R2 | samtools sort > $datadir/$exp.bam
+    # bwa mem -v 1 -t $numbproc $refgen $R1 $R2 > $datadir/$exp.sam
+    # samtools view -h -Sb $datadir/$exp.sam > $datadir/$exp.bam
+    # rm $datadir/$exp.sam
+    # samtools sort $datadir/$exp.bam -o $datadir/$exp.sorted.bam
+    # mv $datadir/$exp.sorted.bam $datadir/$exp.bam 
     samtools index $datadir/$exp.bam 
 fi
 
